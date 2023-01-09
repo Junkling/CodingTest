91a9fd5011d00c0bc3c87b078de374f5a26ab29c
(Jun) <(ddoly0106@gmail.com)>
2023-01-09 22:51:27 +0900

    달팽이 예제

--- src/codingTest/Snail.java
+++ src/codingTest/Snail.java
@@ -1,5 +1,10 @@
 package codingTest;
 
+import java.util.Arrays;
+
+/**
+ * 달팽이 예제
+ */
 public class Snail {
 
     public void solution(int n) {
@@ -8,38 +13,68 @@ public class Snail {
 
         int x = 0;
         int y = 0;
+        int z = n;
         int input = 1;
 
-        boolean locateX = false;
-        boolean locateY = false;
-
-
-        while (input <= 25) {
-            if (0<= y&&y < n || ints[x][y] != 0) {
-                ints[x][y] = input;
-                if (locateY == false) {
-                    y++;
-                } else {
-                    y--;
-                }
-            } else{
-                if (locateY == false) {
-                    locateY = true;
-                }else{
-                    locateY = false;
-                }
-                    if (locateX == false) {
-                        x++;
+        for (int i = 0; i < (2*n)-1; i++) {
+            switch (i % 4) {
+                case 0:
+                    for (int j = 0; j <z; j++) {
                         ints[x][y] = input;
-                    } else {
-                        x--;
-                        ints[x][y] = input;
+                        input++;
+                        y++;
                     }
-                }
-            input++;
+                    x++;
+                    y--;
+                    z--;
+                    break;
+                case 1:
+                    for (int j = 0; j < z; j++) {
+                        ints[x][y] = input;
+                        input++;
+                        x++;
+                    }
+                    y--;
+                    x--;
+                    break;
+                case 2:
+                    for (int j = 0; j < z; j++) {
+                        ints[x][y] = input;
+                        input++;
+                        y--;
+                    }
+                    x--;
+                    y++;
+                    z--;
+                    break;
+                case 3:
+                    for (int j = 0; j < z; j++) {
+                        ints[x][y] = input;
+                        input++;
+                        x--;
+                    }
+                    x++;
+                    y++;
+                        break;
+            }
         }
+
+        System.out.println("ints = " + Arrays.deepToString(ints));
+        System.out.println();
+        System.out.println("==== result =====");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%3d", ints[i][j]);
+            }
+            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Snail snail = new Snail();
+        snail.solution(5);
 
     }
 
 }
+
